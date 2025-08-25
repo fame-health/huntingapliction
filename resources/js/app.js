71516862c739ad65b997188ee3690c17806b1efb
username-fame-health <romipelajarmr01@gmail.com>
2025-08-25 20:05:16 +0700

    romi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+  
+        document.getElementById('mobile-menu-button').addEventListener('click', () => {
+            const mobileNav = document.getElementById('mobile-nav');
+            mobileNav.classList.toggle('hidden');
+            mobileNav.classList.toggle('animate-slide-in');
+        });
+
